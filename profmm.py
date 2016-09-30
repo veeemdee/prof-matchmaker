515945803b0f4a66a374f8e3ceb5672bcd91a2ac
Duc Vu <vud@uni.edu>
2016-09-30 16:53:38 -0500

    obtain lottery function from AI_HW2, add genetic function

--- profmm.py
+++ profmm.py
@@ -56,14 +56,73 @@ def evaluateChromosome(chromosome):
                 pairList.append((profA, profB))
     return score
 
+def lotteryPick(popSize):
+    """   Description:    Helper function that picks an index of a chromosome with
+                          priority on better-fit ones
+          Input:  popSize(int)    - number of chromosomes in the population
+          Output: index of the selected chromosomes
+    """
+    first = random.randint(0, popSize-1)
+    second = random.randint(0, popSize-1)
+    return min(first, second)
+
+def lotterySystem(popSize, geneticFunction, count):
+    """   Description:    Implementation of a lottery system using tournament selection method
+          Input:  popSize(int)        - number of chromosomes in the population
+                  geneticFunction(str)- string indicating which genetic function is in context
+                  count(int)          - number of chromosomes to undergo the genetic operation
+          Output: a list containing indices of chromosomes in the population that have been
+                  selected to undergo the genetic function
+    """
+    lotteryList = []
+    if geneticFunction == 'crossover':
+        for index in range(count):
+            firstPick = lotteryPick(popSize)
+            secondPick = lotteryPick(popSize)
+            while secondPick == firstPick:
+                secondPick = lotteryPick(popSize)
+            lotteryList.append(firstPick)
+            lotteryList.append(secondPick)
+    else:
+        for index in range(count):
+            currentPick = lotteryPick(popSize)
+            lotteryList.append(currentPick)
+    return lotteryList
+
+def crossover(chromosome1, chromosome2):
+    crosspoint = random.randint(1, NUM_OF_SESSION-1)
+    newChromosome1 = chromosome1[:crosspoint]+chromosome2[crosspoint:]
+    newChromosome2 = chromosome2[:crosspoint]+chromosome1[crosspoint:]
+    return newChromosome1, newChromosome2
+
+def mutation(chromosome):
+    def chromoSwap(chromosome, sessionIndex, profIndexA, profIndexB):
+        if profIndexA > profIndexB:
+            tmp = profIndexA
+            profIndexA = profIndexB
+            profIndexB = tmp
+        session = chromosome[sessionIndex]
+        newSession = (session[:profIndexA] + [session[profIndexB]] +
+                      session[profIndexA+1:profIndexB] +
+                      [session[profIndexA]] + session[profIndexB+1:])
+        newChromosome = (chromosome[:sessionIndex] + newSession +
+                         chromosome[sessionIndex + 1:])
+        return newChromosome
+        
+    randomSessionIndex = random.randint(0, NUM_OF_SESSION-1)
+    randomProfIndexA = random.randint(0, NUM_OF_PROF-1)
+    print("A",randomProfIndexA)
+    randomProfIndexB = randomProfIndexA
+    while (randomProfIndexA // 2) == (randomProfIndexB // 2):
+        print("B",randomProfIndexB)
+        randomProfB = random.randint(0, NUM_OF_PROF-1)
+    return chromoSwap(chromosome, randomSessionIndex, randomProfIndexA, randomProfIndexB)
+    
+def selection(chromosome):
+    return chromosome
+
 def test():
-    popSize = 10
-    myPopulation = generatePopulation(popSize)
-    for chromosome in myPopulation:
-        score = evaluateChromosome(chromosome)
-        print(chromosome, score)
-    print("-----")
-    myPopulation = sortPopulation(myPopulation)
-    for chromosome in myPopulation:
-        score = evaluateChromosome(chromosome)
-        print(chromosome, score)
+    aChromo = createChromosome()
+    print(aChromo)
+    newChromo = mutation(aChromo)
+    print(newChromo)
